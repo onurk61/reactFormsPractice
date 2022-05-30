0c31578950e93f139201c5de375a9abc501b37a5
Onur Kanca <onurkanca3@gmail.com>
2022-05-30 17:47:16 +0300

    everything changed

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,52 +1,92 @@
 import React, { useState } from "react";
 
-const SimpleInput = props => {
-    const [enteredUserName, setEnteredUserName] = useState('');
-    const [enteredUserNameIsTouched, setEnteredUserNameIsTouched] = useState(false);
-    
-    const enteredUserNameIsValid = enteredUserName.trim() !== '';
-    const userNameInputIsvalid = !enteredUserNameIsValid && enteredUserNameIsTouched;
+const SimpleInput = (props) => {
+  const [nameEntered, setNameEntered] = useState("");
+  const [mailEntered, setMailEntered] = useState("");
+  const [isValid, setIsvalid] = useState(false);
+  const [mailValid, setMailvalid] = useState(false);
 
-    let formIsValid = false;
+  const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-    if(enteredUserNameIsValid){
-        formIsValid = true;
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (nameEntered.trim() === "" || mailEntered.trim() === '') {
+      setIsvalid(true);
+      setMailvalid(true);
+      return;
     }
 
-    const formSubmitHandler = event => {
-        event.preventDefault();
+    setNameEntered("");
+    setMailEntered("");
+    setIsvalid(false);
+    setMailvalid(false);
+  };
 
-        setEnteredUserNameIsTouched(true);
+  const nameChangeHandler = (event) => {
+    setNameEntered(event.target.value);
 
-        if(!enteredUserNameIsValid){
-            return;
-        }
-        setEnteredUserName('');
-        setEnteredUserNameIsTouched(false);
+    if (event.target.value.length) {
+      setIsvalid(false);
     }
+  };
 
-    const userNameChangeHandler = event => {
-        setEnteredUserName(event.target.value);
+  const nameBlurHandler = (event) => {
+    if (nameEntered.trim().length) {
+      setIsvalid(false);
+      return;
     }
+    setIsvalid(true);
+  };
 
-    const userNameBlurHandler = event => {
-        setEnteredUserNameIsTouched(true);
+  const userMailHandler = (event) => {
+    setMailEntered(event.target.value);
+
+    if (event.target.value.length) {
+      setMailvalid(false);
     }
+  };
 
-    const userNameInputClasses = enteredUserNameIsValid ? 'form-control invalid' : 'form-control';
+  const mailBlurHandler = (event) => {
+    if (mailEntered.trim().length && mailEntered.match(mailFormat)) {
+        setMailvalid(false);
+        return;
+      }
+      setMailvalid(true);
+  }
 
-    return (
-        <form onSubmit={formSubmitHandler}>
-            <div className="form-control">
-                <label htmlFor="userName">User Name</label>
-                <input id='userName' type='text' onChange={userNameChangeHandler} onBlur={userNameBlurHandler} value={enteredUserName} />
-                {userNameInputIsvalid && <p className="error-text">Name must not be empty !</p>}
-            </div>
-            <div className="form-actions">
-                <button>Submit</button>
-            </div>
-        </form>
-    );
-}
+  const nameInputClasses = isValid ? "form-control invalid" : "form-control";
+  const mailInputClasses = mailValid ? "form-control invalid" : "form-control";
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor="userName">Name</label>
+        <input
+          id="userName"
+          type="text"
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={nameEntered}
+        />
+        {isValid && <p className="error-text">Name must not be empty !</p>}
+      </div>
+      <div className={mailInputClasses}>
+        <label htmlFor="userMail">Email</label>
+        <input
+          id="userMail"
+          type="email"
+          onChange={userMailHandler}
+          onBlur={mailBlurHandler}
+          value={mailEntered}
+        />
+        {mailValid && <p className="error-text">Mail must not be empty and Please Enter Valid Email Address !</p>}
+      </div>
+      <div className="form-actions">
+        <button>Submit</button>
+      </div>
+    </form>
+  );
+};
 
 export default SimpleInput;
